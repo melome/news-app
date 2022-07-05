2ac4430845ad4628d12cff9ab025c451d096d82d
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-07-05 12:19:34 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
+
+  const fetchNews = () => {
+    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    .then(result => result.json())
+    .then(data => setNews(data.hits))
+    .catch(error => console.log(error));
+  };
+  
+  useEffect(() => {
+    fetchNews()
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>News</h1>
+      {news.map((n, index) => (<p key={index}>{n.title}</p>))}
     </div>
-  );
+  )
 }
 
 export default App;
